eeeceaa439a03d3a9ea495a0daa12104fd4668fc
Ana Lucia Arias <ariasdelosheros@gmail.com>
2022-11-02 15:05:33 -0300

    pr utn

---
+++ paractica1.js
@@ -0,0 +1,17 @@
+const nombre= prompt('Ingrese su nombre');
+const edad= prompt('Ingrese su edad');
+document.write(`hola ${nombre} asique tienes ${edad}<br>`);
+const valor= parseInt(prompt('Ingrese su primer valor'));
+const valor2= parseInt(prompt('ingrese su segundo valor'));
+const suma= valor+valor2;
+document.write(suma + '<br>')
+
+const producto= valor *valor2;
+document.write(producto+ '<br>');
+ 
+const valores=[2,'berni',30,'fonsi',50,'ana','feli'];
+const multiArray= valores[0] *valores[2];
+document.write(multiArray +'<br>');
+const ultimo=valores[valores.length-1];
+document.write(ultimo)
+
